1481a8406663b8e00967b32d7569b037c2c346fa
Ring-wdr <akswnd55@gmail.com>
2023-01-01 21:00:33 +0900

    feat: navbar 추가가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,17 @@
+import { RouteArr } from '../link/Link';
+import { Link } from 'react-router-dom';
+import styles from './css/navbar.module.css';
+
 export const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className={styles.navbar}>
+      {RouteArr.map(([key, path]) => (
+        <div className={styles.link} key={key}>
+          <Link to={path} style={{ textTransform: 'uppercase' }}>
+            {path.substring(1) || 'HOME'}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
