9d1dc9196a12bfce835039d453d3690002d18dab
Ring-wdr <akswnd55@gmail.com>
2023-01-01 21:02:08 +0900

    feat: recoil state 관리 실험

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,18 @@
+import React from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { textState } from '../recoil/text';
+
 export const Home = () => {
-  return <div>Home</div>;
+  const value = useRecoilValue(textState);
+  const setValue = useSetRecoilState(textState);
+
+  const changeValue = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(() => `${e.target.value}`);
+
+  return (
+    <div>
+      <div>value Test: {value}</div>
+      <input onChange={changeValue} placeholder='input text' />
+    </div>
+  );
 };
