7955888688c53d148a13a1c36a2ba7a71c324236
Ring-wdr <akswnd55@gmail.com>
2023-01-03 23:05:19 +0900

    remove: 주석 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,11 @@
-// import { useRecoilValue, useSetRecoilState } from 'recoil';
-// import { textState } from '../recoil/text';
 import styles from './css/home.module.css';
 
 export const Home = () => {
-  // const value = useRecoilValue(textState);
-  // const setValue = useSetRecoilState(textState);
-
-  // const changeValue = (e: React.ChangeEvent<HTMLInputElement>) =>
-  //   setValue(() => `${e.target.value}`);
-
   return (
     <div className={styles.main}>
       <div className={styles.greeting}>
         <span>LE SSERAFIM SHOP</span>에 오신 것을 환영합니다!
       </div>
-      {/* <div>value Test: {value}</div>
-      <input onChange={changeValue} placeholder='input text' /> */}
       <div className={styles.albums}>
         <img
           src='https://sourcemusic.com/resources/discography/cfae49df-6597-465f-b21f-7b7597560ed1.png'
